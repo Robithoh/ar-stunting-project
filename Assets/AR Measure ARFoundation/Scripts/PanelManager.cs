a6856fb9712ca3102936f25fd864363a430d9434
Tegar Nur Hidayat <nhtegar@gmail.com>
2024-10-17 14:43:24 +0700

    done change error with pop up

--- Assets/AR Measure ARFoundation/Scripts/PanelManager.cs
+++ Assets/AR Measure ARFoundation/Scripts/PanelManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO.Compression;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PanelManager : MonoBehaviour
 {
